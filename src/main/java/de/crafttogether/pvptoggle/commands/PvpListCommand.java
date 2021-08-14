d7d5d06aa24196be0ef1bc8db3e9286bad6f5e57
DevKaai <k-m@outlook.de>
2021-08-14 13:38:43 +0200

    TODO: BungeeCord getPlayerList

--- src/main/java/de/crafttogether/pvptoggle/commands/PvpListCommand.java
+++ src/main/java/de/crafttogether/pvptoggle/commands/PvpListCommand.java
@@ -1,77 +1,59 @@
 package de.crafttogether.pvptoggle.commands;
 
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
 import de.crafttogether.pvptoggle.PvPTogglePlugin;
-import de.crafttogether.pvptoggle.util.Util;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class PvpListCommand implements TabExecutor {
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
 
         FileConfiguration config = PvPTogglePlugin.getInstance().getConfig();
 
-        if (sender instanceof Player) {
-            Player player = (Player) sender;
+        if (sender instanceof Player player) {
             if (!sender.hasPermission("pvptoggle.pvp.list")) {
-                sender.sendMessage(Util.format(config.getString("Message.PvP_NoPerm")));
+                sender.sendMessage(Objects.requireNonNull(config.getString("Message.PvP_NoPerm")));
                 return false;
             }
-            player.sendMessage(Util.format(config.getString("Message.PvP_List")));
-			/*
-			if(!PvPTogglePlugin.getInstance().getPvplist().isEmpty()) {
-				for (Player current : Bukkit.getOnlinePlayers()) {
-					if(PvPTogglePlugin.getInstance().getPvplist().contains(current.getUniqueId()) ) {
-						player.sendMessage("§c" + current.getName());
-					}
-				}
-			} else
-				player.sendMessage(Util.format(config.getString("Message.PvP_Nobody")));
-		}*/
 
-			/* TODO: MYSQL
-			MySQLHandler mySQL = PvPTogglePlugin.getInstance().getMySQLHandler();
-			if(mySQL == null) {
-				player.sendMessage(Util.format("Dein PvP Status konnte nicht geändern werden"));
-				return false;
-			}
-			mySQL.queryAsync(("SELECT `playername` FROM `" + config.getString("MySQL.Database") + "`.`pvplist` WHERE `pvp` = 1"), ((result, thrown) -> {
+            ByteArrayDataOutput out = ByteStreams.newDataOutput();
+            out.writeUTF("Subchannel");
+            out.writeUTF("Argument");
 
-				if(thrown == null) {
-					try {
-						if (result.next()) {
-							do {
-								player.sendMessage("§c" + result.getString("playername"));
-							} while (!result.next());
-						} else {
-							player.sendMessage(Util.format(config.getString("Message.PvP_Nobody")));
-						}
-					} catch (SQLException ex) {
-						ex.printStackTrace();
-					}
-				}
+            // If you don't care about the player
+            // Player player = Iterables.getFirst(Bukkit.getOnlinePlayers(), null);
+            // Else, specify them
+            Player player2 = Bukkit.getPlayerExact("Example");
 
-				// TODO: Close ResultSet(?)
-				try {
-					result.close();
-				} catch (SQLException ex) {
-					ex.printStackTrace();
-				}
-			}));
-			 */
+            player2.sendPluginMessage(PvPTogglePlugin.getInstance(), "BungeeCord", out.toByteArray());
+
+            if (!PvPTogglePlugin.getPvpList().isEmpty()) {
+                for (Player current : Bukkit.getOnlinePlayers()) {
+                    if (PvPTogglePlugin.getPvpList().contains(current.getUniqueId())) {
+                        player.sendMessage("§c" + current.getName());
+                    }
+                }
+            } else
+                player.sendMessage(Objects.requireNonNull(config.getString("Message.PvP_Nobody")));
         }
         return false;
     }
 
     @Override
-    public List<String> onTabComplete(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
+    public List<String> onTabComplete(@NotNull CommandSender arg0, @NotNull Command arg1, @NotNull String arg2, String[] arg3) {
         return new ArrayList<>();
     }
 }
