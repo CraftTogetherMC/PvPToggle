724b2b9d6037b6b4e3f750510bff2a5358964a92
DevKaai <k-m@outlook.de>
2022-02-06 18:05:23 +0100

    extract method

--- src/main/java/de/crafttogether/pvptoggle/util/Util.java
+++ src/main/java/de/crafttogether/pvptoggle/util/Util.java
@@ -1,6 +1,14 @@
 package de.crafttogether.pvptoggle.util;
 
+import de.crafttogether.pvptoggle.PvPTogglePlugin;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Util {
 
@@ -38,5 +46,50 @@ public class Util {
             case "Skeleton Horse" -> "Skelett Pferd";
             default -> entityName;
         };
+    }
+
+    public static void sendPvplistFromDatabaseToPlayer(@NotNull Player player, List<String> playerList) {
+        Plugin plugin = PvPTogglePlugin.getInstance();
+
+        if (plugin.getConfig().getBoolean("Settings.Debug"))
+            plugin.getLogger().info("[MySQL]: Select all playernames where pvpstate = 1 ...");
+
+        MySQLAdapter.MySQLConnection connection = PvPTogglePlugin.getMySQL().getConnection();
+
+        connection.queryAsync("SELECT `playername` FROM `%s` WHERE `pvpstate` = '1'", (err, result) -> {
+            if (err != null)
+                plugin.getLogger().warning("[MySQL]: " + err.getMessage());
+
+            try {
+                List<String> pvplist = new ArrayList<>();
+                while (result.next()) {
+                    pvplist.add(result.getString("playername"));
+                }
+                player.sendMessage(PvPTogglePlugin.getPreloadConfig().getString("Message.PvP_List"));
+
+                List<String> finalPvpList = new ArrayList<>();
+
+                if (!pvplist.isEmpty()) {
+                    for (String pvplistPlayer : pvplist) {
+                        for (String onlinePlayer : playerList) {
+                            if (pvplistPlayer.equals(onlinePlayer))
+                                finalPvpList.add(pvplistPlayer);
+                        }
+                    }
+                    if (!finalPvpList.isEmpty()) {
+                        for (String s : finalPvpList) {
+                            player.sendMessage("§8-§c " + s);
+                        }
+                    } else {
+                        player.sendMessage(PvPTogglePlugin.getPreloadConfig().getString("Message.PvP_Nobody"));
+                    }
+                } else {
+                    player.sendMessage(PvPTogglePlugin.getPreloadConfig().getString("Message.PvP_Nobody"));
+                }
+            } catch (SQLException e) {
+                PvPTogglePlugin.getInstance().getLogger().warning(e.getMessage());
+            }
+            connection.close();
+        }, connection.getTablePrefix() + "pvplist");
     }
 }
