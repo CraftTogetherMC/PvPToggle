9c852cefbef3bae9fef7829198e3da6d3c903e0a
Kai Hoffmann <58406743+DevKaai@users.noreply.github.com>
2022-09-19 17:40:42 +0200

    to master

--- src/main/java/de/crafttogether/pvptoggle/listener/OnPluginMessageReceived.java
+++ src/main/java/de/crafttogether/pvptoggle/listener/OnPluginMessageReceived.java
@@ -1,25 +1,17 @@
 package de.crafttogether.pvptoggle.listener;
 
-import com.google.common.collect.Iterables;
 import com.google.common.io.ByteArrayDataInput;
-import com.google.common.io.ByteArrayDataOutput;
 import com.google.common.io.ByteStreams;
 import de.crafttogether.pvptoggle.PvPTogglePlugin;
-import de.crafttogether.pvptoggle.util.MySQLAdapter;
-import org.bukkit.Bukkit;
+import de.crafttogether.pvptoggle.pvplist.PvPListSQL;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.messaging.PluginMessageListener;
 import org.jetbrains.annotations.NotNull;
-
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.IOException;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import java.util.Arrays;
 
 public class OnPluginMessageReceived implements PluginMessageListener {
 
@@ -35,48 +27,7 @@ public class OnPluginMessageReceived implements PluginMessageListener {
             if (server.equals("ALL")) {
                 String[] playerList = in.readUTF().split(", ");
 
-                Plugin plugin = PvPTogglePlugin.getInstance();
-
-                if (plugin.getConfig().getBoolean("Settings.Debug"))
-                    plugin.getLogger().info("[MySQL]: Select all playernames where pvpstate = 1 ...");
-
-                MySQLAdapter.MySQLConnection connection = PvPTogglePlugin.getMySQL().getConnection();
-
-                connection.queryAsync("SELECT `playername` FROM `%s` WHERE `pvpstate` = '1'", (err, result) -> {
-                    if (err != null)
-                        plugin.getLogger().warning("[MySQL]: " + err.getMessage());
-
-                    try {
-                        List<String> pvplist = new ArrayList<>();
-                        while (result.next()) {
-                            pvplist.add(result.getString("playername"));
-                        }
-                        player.sendMessage(PvPTogglePlugin.getPreloadConfig().getString("Message.PvP_List"));
-
-                        List<String> finalPvpList = new ArrayList<>();
-
-                        if (!pvplist.isEmpty()) {
-                            for (String pvplistPlayer : pvplist) {
-                                for (String onlinePlayer : playerList) {
-                                    if (pvplistPlayer.equals(onlinePlayer))
-                                        finalPvpList.add(pvplistPlayer);
-                                }
-                            }
-                            if (!finalPvpList.isEmpty()) {
-                                for (String s : finalPvpList) {
-                                    player.sendMessage("§8-§c " + s);
-                                }
-                            } else {
-                                player.sendMessage(PvPTogglePlugin.getPreloadConfig().getString("Message.PvP_Nobody"));
-                            }
-                        } else {
-                            player.sendMessage(PvPTogglePlugin.getPreloadConfig().getString("Message.PvP_Nobody"));
-                        }
-                    } catch (SQLException e) {
-                        PvPTogglePlugin.getInstance().getLogger().warning(e.getMessage());
-                    }
-                    connection.close();
-                }, connection.getTablePrefix() + "pvplist");
+                PvPListSQL.sendMessagePvplist(player, Arrays.stream(playerList).toList());
             }
         }
         else if (subchannel.equals("pvptoggle")) {
@@ -98,7 +49,7 @@ public class OnPluginMessageReceived implements PluginMessageListener {
                 if (plugin.getConfig().getBoolean("Settings.Debug"))
                     plugin.getLogger().info("[MySQL]: Select uuid, pvpstate ...");
 
-                PvPTogglePlugin.getInstance().updatePvplist();
+                PvPListSQL.updatePvplist();
             }
         }
     }
